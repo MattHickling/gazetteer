c3d4fc2a9f902e5e37f3496dff868516bae5b12e
MattHickling <62998487+MattHickling@users.noreply.github.com>
2023-06-06 21:04:45 +0100

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-$username = 'matt1883';
-
-$apiKey = 'aa2b92718386464a8fb461ba54715c53'; 
-
-$news_api_key = 'd2d3fb528c418ea65db242226aac02dd';
-
-$weather_api_key = '8b8b3b8efd5719cc409baad20eeec20a';
